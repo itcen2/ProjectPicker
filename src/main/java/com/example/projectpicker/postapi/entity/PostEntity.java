29a06f08c67ae0834052b81a36194bace4e9310f
ENFJ <33335762+choisunghwan@users.noreply.github.com>
2023-02-04 21:24:31 +0900

    # PostEntity 수정, application.properties 수정(db 설정)

--- src/main/java/com/example/projectpicker/postapi/entity/PostEntity.java
+++ src/main/java/com/example/projectpicker/postapi/entity/PostEntity.java
@@ -14,21 +14,30 @@ import java.time.LocalDateTime;
 @Builder
 
 @Entity
-@Table(name = "tbl_board")
+@Table(name = "tbl_post")
 public class PostEntity {
 
     @Id
     @GeneratedValue(generator = "system-uuid")
     @GenericGenerator(name = "system-uuid", strategy = "uuid") // 전략 = "중복되지 않는 id" 설정
+    private String postId; //게시판 식별
 
-    private Long postId;
-    private String title;
-    private String content;
+    @Column(nullable = false, name = "post_title") // 널값 허용X
+    private String title; // 제목
+
+    @Column(nullable = false, name = "post_content")
+    private String content; // 내용
 
     @CreationTimestamp
-    private LocalDateTime create_date;
+    @Column(name = "create_date")
+    private LocalDateTime createdate; // 게시글 생성시간
+
+    @Column(name = "modify_date")
+    private LocalDateTime modifyDate; // 게시글 수정 시간
+
 
 //    @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "user")
+//    @JoinColumn(name = "user_id",insertable=false,updatable=false)
+//    private String user_Id; // 회원 식별
 
 }

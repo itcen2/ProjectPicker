314f45274f37407e91a307df6bb8dc2d5affb514
ENFJ <33335762+choisunghwan@users.noreply.github.com>
2023-02-05 14:28:06 +0900

    # 관리자,일반회원 레포지터리 생성

---
+++ src/main/java/com/example/projectpicker/userapi/repository/AdminRepository.java
@@ -0,0 +1,21 @@
+package com.example.projectpicker.userapi.repository;
+
+import com.example.projectpicker.userapi.entity.AdminEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface AdminRepository extends JpaRepository<AdminEntity,String>{
+
+    // 이메일로 회원 조회
+    AdminEntity findByEmail(String email);
+
+
+    // 이메일 중복 검사
+    boolean existsByEmail(String email);
+}
+
+/*
+고민
+1. 관리자는 이메일을 통한 관리자회원을 조회할것인지..
+2. 관리자도 이메일 중복검사 필요하지 않은지..
+*/

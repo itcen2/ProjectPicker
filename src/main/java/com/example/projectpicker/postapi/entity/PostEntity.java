e38490e446ace938ca24e3e6314f3ac470d7b295
choisunghwan <dute7570@naver.com>
2023-02-07 10:22:27 +0900

    # 게시글 엔티티 수정

--- src/main/java/com/example/projectpicker/postapi/entity/PostEntity.java
+++ src/main/java/com/example/projectpicker/postapi/entity/PostEntity.java
@@ -33,16 +33,20 @@ public class PostEntity {
     private String postContent; // 게시글 내용
 
     @CreationTimestamp // INSERT 시점에 서버시간을 자동으로 입력
-    @Column(name = "create_date")
-    private LocalDateTime createdate; // 게시글 생성시간
+//    @Column(name = "create_date")
+    private LocalDateTime createDate; // 게시글 생성시간
 
-    @UpdateTimestamp // UPDATE시점에 서버시간을 자동으로 입력
+//    @UpdateTimestamp // UPDATE 시점에 서버시간을 자동으로 입력
+    @CreationTimestamp
     @Column(name = "modify_date")
     private LocalDateTime modifyDate; // 게시글 수정 시간
 
 
+    /**
+     회원 와 관계 설정
+     *
+     */
 
-    // 회원 와 관계 설정
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id",insertable = false,updatable = false)
     private UserEntity user;
@@ -52,14 +56,21 @@ public class PostEntity {
     private String user_Id; // 회원 식별
 
     @Column(name = "user_email")
-    private String email;
+    private String userEmail;
 
     @Column(name = "user_name")
     private String userName;
 
-    // 해시태그와 관계형 매핑
+
+    /**
+     * 해시태그와 관계형 매핑
+      */
+
     @OneToMany(mappedBy = "post")
     private List<HashTagEntity> hashTags = new ArrayList<>(); //해시태그 목록
 
 
+    // 일반 회원이 게시글 등록시, 관리자가 게시글 등록 허용여부에 사용되는 엔티티
+    @Column
+    private boolean allow;
 }

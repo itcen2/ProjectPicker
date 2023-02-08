45af3ac145f4789a045ea18693397797fab66847
choisunghwan <dute7570@naver.com>
2023-02-08 10:56:30 +0900

    # CommentEntity 생성 및 회원,게시판 매핑 설정

---
+++ src/main/java/com/example/projectpicker/comment/entity/CommentEntity.java
@@ -0,0 +1,45 @@
+package com.example.projectpicker.comment.entity;
+
+import com.example.projectpicker.post.entity.PostEntity;
+import com.example.projectpicker.user.entity.UserEntity;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter @Setter @ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "commentId")
+@Entity
+@Builder
+@Table(name = "tbl_comment")
+public class CommentEntity {
+
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
+    private String commentId; // 댓글 식별코드
+    private String comment; // 댓글 내용
+
+    private LocalDateTime createAt; // 댓글 생성 시간
+
+
+    /**
+     회원 와 관계 설정
+     */
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", insertable = false,updatable = false)
+    private UserEntity user;
+
+
+    /**
+     게시판 와 관계 설정
+     */
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private PostEntity post;
+
+}
+

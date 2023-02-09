f2f44d8993732c368f58e702c3b1c809e330c038
choisunghwan <dute7570@naver.com>
2023-02-09 09:01:05 +0900

    # 해시태그  엔티티 수정

--- src/main/java/com/example/projectpicker/post/service/PostService.java
+++ src/main/java/com/example/projectpicker/post/service/PostService.java
@@ -30,6 +30,7 @@ import static java.util.stream.Collectors.toList;
 @Service
 @Slf4j
 @RequiredArgsConstructor
+@Transactional
 public class PostService {
 
     private final PostRepository postRepository;
@@ -108,6 +109,78 @@ public class PostService {
 
         return listResponseDTO;
     }
+
+
+    /**
+     * 특정 검색 리스트 조회 (추가한거)
+     */
+    public PostListResponseDTO searchList(PageRequestDTO pageRequestDTO, String string) {
+
+        Pageable pageable = PageRequest.of(
+                pageRequestDTO.getPage() - 1,
+                pageRequestDTO.getSizePerPage(),
+                Sort.Direction.DESC,
+                "createDate"
+        );
+
+        // final Page<PostEntity> pageData = postRepository.findByAllowTrueAndPostTitleContaining(string, pageable);
+        final Page<PostEntity> pageData = postRepository.findByPostTitleContaining(string, pageable);
+        List<PostEntity> list = pageData.getContent();
+
+        if (list.isEmpty()) {
+            throw new RuntimeException("조회 결과가 없습니다.");
+        }
+
+        // 엔터티 리스트를 DTO리스트로 변환해서 클라이언트에 응답
+        List<PostResponseDTO> responseDTOList = list.stream()
+                .map(PostResponseDTO::new)
+                .collect(toList());
+
+        PostListResponseDTO listResponseDTO = PostListResponseDTO.builder()
+                .count(responseDTOList.size())
+                .pageInfo(new PageResponseDTO<PostEntity>(pageData))
+                .posts(responseDTOList)
+                .build();
+
+        return listResponseDTO;
+    }
+
+    /**
+     * 특정 해시태그 검색 리스트 조회 중간처리 (추가한거)
+     */
+    public PostListResponseDTO searchHashTagList(String keyword1, String keyword2, PageRequestDTO pageRequestDTO) {
+
+        List<String> postId = postRepository.HashTagsSearch(keyword1, keyword2);
+
+        Pageable pageable = PageRequest.of(
+                pageRequestDTO.getPage() - 1,
+                pageRequestDTO.getSizePerPage(),
+                Sort.Direction.DESC,
+                "create_Date"
+        );
+
+        //final Page<PostEntity> pageData = postRepository.findByAllowTrueAndPostTitleContaining(string, pageable);
+        final Page<PostEntity> pageData = postRepository.findPostId(postId, pageable);
+        List<PostEntity> list = pageData.getContent();
+
+        if (list.isEmpty()) {
+            throw new RuntimeException("조회 결과가 없습니다.");
+        }
+
+        // 엔터티 리스트를 DTO리스트로 변환해서 클라이언트에 응답
+        List<PostResponseDTO> responseDTOList = list.stream()
+                .map(PostResponseDTO::new)
+                .collect(toList());
+
+        PostListResponseDTO listResponseDTO = PostListResponseDTO.builder()
+                .count(responseDTOList.size())
+                .pageInfo(new PageResponseDTO<PostEntity>(pageData))
+                .posts(responseDTOList)
+                .build();
+
+        return listResponseDTO;
+    }
+
 
 
     /**

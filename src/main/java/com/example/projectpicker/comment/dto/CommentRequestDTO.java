e26bbad94efbefc2e3d2d3c753d0a613ca7e42e3
choisunghwan <dute7570@naver.com>
2023-02-09 16:32:50 +0900

    # 댓글 DTO

--- src/main/java/com/example/projectpicker/comment/dto/CommentRequestDTO.java
+++ src/main/java/com/example/projectpicker/comment/dto/CommentRequestDTO.java
@@ -14,21 +14,12 @@ import java.time.LocalDateTime;
 @EqualsAndHashCode
 @Builder
 public class CommentRequestDTO {
-        private String commentId;
         private String comment;
-        @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
-        private LocalDateTime createAt;
-        @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
-        private LocalDateTime modifyAt;
-        private UserEntity userEntity;
-        private PostEntity postEntity;
 
-        public CommentEntity toEntity() {
+        // 댓글 요청 DTO ( DTO -- > Entity)
+        public CommentEntity toEntity(UserEntity userEntity,PostEntity postEntity) {
             return  CommentEntity.builder()
-                    .commentId(commentId)
                     .comment(comment)
-                    .createAt(createAt)
-                    .modifyAt(modifyAt)
                     .userEntity(userEntity)
                     .postEntity(postEntity)
                     .build();
--- src/main/java/com/example/projectpicker/comment/dto/CommentResponseDTO.java
+++ src/main/java/com/example/projectpicker/comment/dto/CommentResponseDTO.java
@@ -14,23 +14,40 @@ import java.time.LocalDateTime;
 @EqualsAndHashCode
 @Builder
 public class CommentResponseDTO {
-   private String commentId;
-   private String comment;
-   @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
-   private LocalDateTime createAt;
-   @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
-   private LocalDateTime modifyAt;
-   private String userName;
-   private String userEmail;
-   private String postId;
 
-   public CommentResponseDTO(CommentEntity commentEntity) {
+    /**
+     * 댓글(comment)
+     */
+
+    private String commentId; // 댓글 식별 아이디(코드)
+   private String comment; // 댓글 내용
+   @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+   private LocalDateTime createAt; // 댓글 생성 시간
+   @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+   private LocalDateTime modifyAt; // 댓글 수정 시간
+
+    /**
+     * 회원(user)
+     */
+    private String userName; // 회원 이름
+   private String userEmail; // 회원 이메일
+
+    /**
+     * 게시글(post)
+     */
+    private String postId; // 게시글 식별 아이디(코드)
+
+
+    /**
+     * DTO <-- Entity
+     */
+    public CommentResponseDTO(CommentEntity commentEntity) {
        this.commentId = commentEntity.getCommentId();
        this.comment = commentEntity.getComment();
        this.createAt = commentEntity.getCreateAt();
        this.modifyAt = commentEntity.getModifyAt();
        this.userName = commentEntity.getUserEntity().getUserName();
-       this.userEmail = commentEntity.getUserEntity().getEmail();
+       this.userEmail = commentEntity.getUserEntity().getUserEmail();
        this.postId = commentEntity.getPostEntity().getPostId();
    }
 }

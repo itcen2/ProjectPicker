6c75329964ff333dde1f352b1e57dfc23ba615df
choisunghwan <dute7570@naver.com>
2023-02-09 16:27:51 +0900

    # 댓글 엔티티 확인

--- src/main/java/com/example/projectpicker/comment/entity/CommentEntity.java
+++ src/main/java/com/example/projectpicker/comment/entity/CommentEntity.java
@@ -34,13 +34,20 @@ public class CommentEntity {
     @UpdateTimestamp
     private LocalDateTime modifyAt;  // 댓글 수정 시간
 
-    @ManyToOne
+    /**
+     * 회원 와 관계 설정
+     */
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private UserEntity userEntity;
 
-    @ManyToOne
+    /**
+     * 게시판 와 관계 설정
+     */
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "post_id")
     private PostEntity postEntity;
+
 
     // 댓글 수정을 위한 setter
     public void update(String comment) {
